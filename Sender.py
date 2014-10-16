c56dc09682c928a0aede17080a90828c6e022da7
daviis <isaac@luther.edu>
2014-10-16 18:08:15 -0500

    used a dirty hack to add in a global var to the rdt_send fun after creating the function

--- Sender.py
+++ Sender.py
@@ -6,29 +6,42 @@ Created on Oct 9, 2014
 import socket, traceback
 from udt import *
 
-host = ''                               # Bind to all interfaces
-port = 55000
+global pktNum 
 pktNum = 0
-estring = "Hi Brad"
 
-
-while True:
-    try:
-        pkt = make_pkt(estring,pktNum,host,port)
-        sock = udt_send(pkt) #causes a print
-        pkt,addr = udt_recv(sock)
-        if pkt:
-            if not pkt.is_corrupt or pkt.seqnum != pktNum:
-                if "ACK" in pkt.mess and str(pktNum) in pkt.mess:
-                    pktNum = (pktNum + 1) % 2
-                    print(pkt.mess)
+def rdt_send(someMsg):
+    host = ''                               # Bind to all interfaces
+    port = 55000
+    needToSend = True 
+    global pktNum 
+    while needToSend:
+        try:
+            pkt = make_pkt(someMsg,pktNum,host,port)
+            sock = udt_send(pkt) #causes a print
+            pkt,addr = udt_recv(sock)
+            if pkt:
+                if not pkt.is_corrupt or pkt.seqnum != pktNum:
+                    if "ACK" in pkt.mess and str(pktNum) in pkt.mess:
+                        pktNum = (pktNum + 1) % 2
+                        print(pkt.mess)
+                        needToSend = False
+                    else:
+                        print("got something other than ACK for ", pktNum)
                 else:
-                    print("got something other than ACK for ", pktNum)
+                    print('either corrupt or ack of not ', pktNum)
             else:
-                print('either corrupt or ack of not ', pktNum)
-        else:
-            print('got a timeout resend ', pktNum)
-    except (KeyboardInterrupt, SystemExit):
-        break
-    except:
-        traceback.print_exc()
+                print('got a timeout resend ', pktNum)
+        except (KeyboardInterrupt, SystemExit):
+            break
+        except:
+            traceback.print_exc()
+
+def main():
+    message = "wow this is a long message about nothing important, but hi brad"
+    
+    for word in message.split(sep=" "):
+        rdt_send(word)
+    
+    
+if __name__ == '__main__':
+    main()
